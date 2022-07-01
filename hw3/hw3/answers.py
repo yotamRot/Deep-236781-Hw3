eebae2280cb23b00b9465ba00b1174b918ecbf37
PLIOPS\yotamr <yotamr@pliops.com>
2022-07-01 13:41:43 +0300

    more answers

--- hw3/hw3/answers.py
+++ hw3/hw3/answers.py
@@ -51,9 +51,16 @@ part1_q1 = r"""
 **Your answer:**
 
 We split into sequence because of the following reasons:
-1. Since the corpus might be very large then including all data and RNN network information will not fit in memory. Therfore splitting into sequences will    make the RNN network smaller and require less data avialble in RAM so it will prevent this issue.
+1. Since the corpus might be very large then including all data and RNN network information will not fit in memory. 
+Therefore splitting into sequences will make the RNN network smaller and require less data avialble in RAM so it will prevent this issue.
 
-2. If we train on entire corpus it will cause our RNN network to be very deep. Therfore it might cause vanishing gradient issue that will make our model untrainable.
+2. This might help model not to over fit original corpus because these way each train 
+    iteration that is unique, and in addition words in same sequence or batch have strong relation to each other
+    because they are from same context in corpus so they create good sequential data to train iteration.
+    These way we can prevent over-fit by training in each batch using serial sequences.
+    
+3. If we train on entire corpus it will cause our RNN network to be very deep. 
+Therefore it might cause vanishing gradient issue that will make our model unattainable.
 
 
 """
@@ -62,23 +69,29 @@ part1_q2 = r"""
 **Your answer:**
 
 The model is able to show longer memory because output also depends on hidden state. Those hidden states are effected by previous batches 
-and therfore since current model output is also affected by them it can produce out with longer memory then sequence len.
+and therefore since current model output is also affected by them it can produce out with longer memory then sequence len.
 
 """
 
 part1_q3 = r"""
 **Your answer:**
 As mentioned before since each batch depends on hidden state created by previous batches, The order of batches is importent. 
-Therfore shuffeling the batches will defect the content that is given from the corpus sentence order and the relation between consecutive batches which is expressed in hidden state.
+Therefore shuffling the batches will defect the content that is given from the corpus sentence order and the relation 
+between consecutive batches which is expressed in hidden state.
 """
 
 part1_q4 = r"""
 **Your answer:**
-1. The temperture hyper parameter help us to control the variance of distribution created by sofmax. High values
-gives small variance and more uniform distribution. Therefore when we take temperture lower then 1.0 we can get more 
-probability to higher scored predictions (in the training) and from then get less uniform. 
-2. As we have seen in the graph above, when temperature increase we get distributdion that is closer to uniform. Threfore we are getting closer to random choose of next char because all choises have almost the same probality. So what we have learn has no effect and it will defect learning prcoess.
-3. As we have seen in the graph above, when temperature over decrease our probablilty model created by the softmax will be degenerated and only the highest score predictions will be choosen since its probability is close to 1.
+1. The temperature hyper parameter help us to control the variance of distribution created by sofmax. High values
+gives small variance and more uniform distribution. Therefore when we take temperature lower then 1.0 we can get more get less uniform 
+and more probability to higher scored predictions (in the training) that will reflect the result better. 
+
+2. As we have seen in the graph above, when temperature increase we get distribution that is closer to uniform. 
+Therefore we are getting closer to random choose of next char because all chooses have almost the same probability.
+ So what we have learn has no effect and it will defect learning process.
+ 
+3. As we have seen in the graph above, when temperature over decrease our probability model created by the softmax will 
+be degenerated and only the highest score predictions will be chosen since its probability is close to 1 (deterministic).
 """
 # ==============
 
@@ -105,19 +118,24 @@ def part2_vae_hyperparams():
 part2_q1 = r"""
 **Your answer:**
 $\sigma^2$ hyperparameter as implicit from its name control the variance of the output.
-High values will give images that are more similar to each other, and low values will produce diversed images( different Boshes).
+High values will give images that are more similar to dataset, and low values will produce more diverse with richer variety images( different Boshes).
 Also we can see from the loss function that $\sigma^2$ control how much weight we give to the reconstruction against the KL divergence.
 """
 
 part2_q2 = r"""
 **Your answer:**
-1. Lets start with the reconstruction loss, its purpose to minimize the difference between the input and the output that was constructed by the encoder-decoder.
-Now the second term the KL divergence is used to control the diversity of the outputs that produce by the model (As we will explain in 2).
-2. The KL divergence is a regulization term that tries to minize how prior and post prior distributions are diffrent from each other and this way control the decoder output which is the latent space distribution model.
-Therfore it controls how diversed are our sampels from latent space and how much it is close to normal distibution.
+1. Lets start with the reconstruction loss, its purpose to minimize the difference between the input and the output that 
+was constructed by the encoder-decoder.
+Now the second term the KL divergence is used to control the diversity of the 
+outputs that produce by the model by comparing latent output to wanted distribution (As we will explain in 2).
+
+2. The KL divergence is a regularization term that tries to minimize how prior and post prior distributions are 
+different from each other and this way control the decoder output which is the latent space distribution model.
+Therefore it controls how diverse are our samples from latent space and how much it is close to normal distribution.
 
 3. The benefit of this effect is that it tries to make the model more simple and this way we will avoid overfitting
-(Which may occur in very complex function) and get better result in mapping output point to their latent space    coordinates.
+(Which may occur in very complex function) and get better result in mapping output point to their latent space coordinates.
+
 
 """
 
@@ -186,6 +204,9 @@ def part3_gan_hyperparams():
 
 part3_q1 = r"""
 **Your answer:**
+
+We want to maintain the gradients only in the part when we train the generator.
+
 We have two phases in our training process. First one to train the discriminator and second one to train the generator. 
 In the training process our discriminator is using real image and image that was created by the generator, 
 i.e. the generator is being used in both phases.
@@ -194,6 +215,7 @@ So we don't want to maintain the generator gradients and update it's parameters 
 training process and errors of the discriminator, and vise versa.
 
 In the second part when we try to improve the generator we want to maintain the generator gradients and update it's parameters.
+
 To conclude we want to make sure training parameters of discriminator and generator change only in their training phase
 although the discriminator is using the generator.
 """
@@ -203,8 +225,8 @@ part3_q2 = r"""
 
 1. No. Since the loss function of generator score is based on how many images generated by him was labeled as fake 
    by the discriminator. Therefore if discriminator is bad and labels non realistic fake images as real ones then generator 
-   loss will be very good regardless the images it generates, i.e. generator loss is depended
-   on the discriminator ability to recognize fake images.
+   loss will be very good regardless the images it generates, 
+   i.e. generator loss is depended on the discriminator ability to recognize fake images.
    Therefore stopping training because the result of the generator loss is below a certain value is not a good idea 
    since it does not mean that the images produced are necessarily good as explained above. 
 
@@ -217,10 +239,12 @@ part3_q2 = r"""
 
 part3_q3 = r"""
 **Your answer:**
-
-Using gan our result were sharper and VAE are more realistic.
-Using Vae we got ...
-This can be explained because..
+In output images main difference between Gan and Vae is that Vae produced smoother images.
+Reason for it loss function is more simple and trained on real images that are smooth. Therfore generataed images 
+are less complex which makes the smoother.
+On the other hand Gan model is much more complex (Based on generator and discriminator) , i.e. it 
+can represent much more complex models/functions and therfore it can create much comlex images
+that might look more complex but less smooth and more noisy.
 
 """
 
